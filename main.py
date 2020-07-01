d4fff7ea1ee26101b4da741341c422907bdd8dd0
Krishnan Shankar <krishnans2006@gmail.com>
2020-07-01 10:53:19 -0400

    Wrote introductory code from repl.it

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+pygame.font.init()
+
+W = 800
+H = 600
+win = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Scarlett Inferno")
+clock = pygame.time.Clock()
+
+music = pygame.mixer.music.load() # will be filled in
+pygame.mixer.music.play(-1)
+
+bg = pygame.image.load() # will be filled in
+character = pygame.image.load() # will be filled in
+
+KILLS = 0
