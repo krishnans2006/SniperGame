e4ed1534c2e95a2fcad1eb30c5d8fccd207276e2
Krishnan Shankar <krishnans2006@gmail.com>
2020-07-01 16:36:36 -0400

    Added basic movement logic

--- main.py
+++ main.py
@@ -9,23 +9,36 @@ pygame.display.set_caption("Scarlett Inferno")
 clock = pygame.time.Clock()
 
 bg = pygame.image.load("sniper_bg.jpg")
+bg_pos = [0, 0]
+move_speed = 8
+
 playerImg = pygame.image.load("player.png")
-person_img = pygame.image.load() # will be filled in
+# person_img = pygame.image.load()
 
 KILLS = 0
 
 
 def redraw(win):
-    win.blit(bg, (0, 0))
+    win.blit(bg, bg_pos)
     pygame.display.flip()
 
 
 def main():
+    global bg_pos
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            bg_pos[0] += move_speed
+        if keys[pygame.K_RIGHT]:
+            bg_pos[0] -= move_speed
+        if keys[pygame.K_UP]:
+            bg_pos[1] += move_speed
+        if keys[pygame.K_DOWN]:
+            bg_pos[1] -= move_speed
         redraw(win)
         clock.tick(30)
 
