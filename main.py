25ce124c137921b29ed823643a9d3534809cf328
shivamrcsuri <shivamrcsuri@gmail.com>
2020-07-01 15:24:58 -0400

    one file added!

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import pygame
 pygame.init()
 pygame.font.init()
-pygame.mixer.init()
 
 W = 800
 H = 600
 win = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Scarlett Inferno")
 clock = pygame.time.Clock()
-
-music = pygame.mixer.music.load("music.mp3")
-pygame.mixer.music.set_volume(40)
-pygame.mixer.music.play(-1)
 
 bg = pygame.image.load("sniper_bg.jpg")
 # person_img = pygame.image.load() # will be filled in
