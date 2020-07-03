d3754a7402b24ef4a183b1ef7d32755610487041
shivamrcsuri <shivamrcsuri@gmail.com>
2020-07-03 14:52:27 -0400

    added a red dot for aims

--- classes.py
+++ classes.py
@@ -6,6 +6,8 @@ class Person:
         self.x = x
         self.y = y
         self.img = img
+        self.hitbox = pygame.Rect(self.x, self.y, 83, 200)
+        self.dead = False
 
     def draw(self, win):
         player = pygame.image.load("player.png")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 pygame.init()
 pygame.font.init()
 
@@ -15,9 +16,16 @@ move_speed = 8
 
 player_img = pygame.image.load("player.png")
 person_img = pygame.image.load("person.png")
-
+persons = []
 KILLS = 0
 
+
+def shoot(x, y, persons):
+    # for person in persons:
+    #     if person.hitbox.collidepoint((x, y)):
+    #         person.dead = True
+    pygame.draw.circle(win, (255, 0, 0), (388, 445), 7)
+    pygame.display.flip()
 
 def redraw(win):
     win.blit(bg, bg_pos)
@@ -25,6 +33,7 @@ def redraw(win):
     win.blit(person_img, (50, 50))
     win.blit(font.render(f"Kills: {KILLS}", 1, (255, 255, 255)), (10, 10))
     pygame.display.flip()
+
 
 def main():
     global bg_pos
@@ -42,6 +51,9 @@ def main():
             bg_pos[1] += move_speed
         if keys[pygame.K_DOWN]:
             bg_pos[1] -= move_speed
+        if keys[pygame.K_SPACE]:
+            shoot(500, 325, persons)
+
         redraw(win)
         clock.tick(30)
 
