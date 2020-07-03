c1bb9d952ac01f375cdab8a4a945f76172432632
shivamrcsuri <shivamrcsuri@gmail.com>
2020-07-03 15:21:22 -0400

    added people randomly, and some spawn on your butt

--- main.py
+++ main.py
@@ -27,17 +27,15 @@ pygame.time.set_timer(USEREVENT + 1, 3000)
 
 
 def shoot(x, y, persons):
-    # for person in persons:
-    #     if person.hitbox.collidepoint((x, y)):
-    #         person.dead = True
     pygame.draw.circle(win, (255, 0, 0), (388, 445), 7)
     pygame.display.flip()
 
 
-def redraw(win):
+def redraw(win, persons):
     win.blit(bg, bg_pos)
     win.blit(player_img, (300, H - player_img.get_height()))
-    win.blit(person_img, (50, 50))
+    for person in persons:
+        person.draw(win)
     win.blit(font.render(f"Kills: {KILLS}", 1, (255, 255, 255)), (10, 10))
     pygame.display.flip()
 
@@ -53,7 +51,7 @@ def main():
                 x = random.randint(0, bg.get_width())
                 y = random.randint(0, bg.get_height())
                 persons.append(Person(x, y, person_img))
-                print("New person added at (x,y)")
+                print(f"New person added at {x}, {y}")
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
@@ -70,7 +68,7 @@ def main():
                 bg_pos[1] -= move_speed
         if keys[pygame.K_SPACE]:
             shoot(500, 325, persons)
-        redraw(win)
+        redraw(win, persons)
         clock.tick(30)
 
 
