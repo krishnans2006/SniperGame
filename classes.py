3f0bc771f450c814eaa10e3191bc143810c081eb
Krishnan Shankar <krishnans2006@gmail.com>
2022-01-20 14:49:14 -0500

    features and fixes?

--- classes.py
+++ classes.py
@@ -9,6 +9,7 @@ class Person:
         self.hitbox = pygame.Rect(self.x, self.y, 83, 200)
         self.dead = False
 
-    def draw(self, win):
-        player = pygame.image.load("player.png")
-        win.blit(self.img, (self.x, self.y))
+    def draw(self, win, bg_pos):
+        if not self.dead:
+            win.blit(self.img, (self.x + bg_pos[0], self.y + bg_pos[1]))
+
--- main.py
+++ main.py
@@ -22,21 +22,45 @@ move_speed = 8
 player_img = pygame.image.load("player.png")
 person_img = pygame.image.load("person.png")
 persons = []
-KILLS = 0
-pygame.time.set_timer(USEREVENT + 1, 3000)
+kills = 0
+pygame.time.set_timer(USEREVENT + 1, 8000)
 
 
 def shoot(x, y, persons):
+    global kills
+    for person in persons:
+        if not person.dead and person.hitbox.collidepoint((x, y)):
+            person.dead = True
+            print(f"Killed person at {person.x}, {person.y} by aiming at {x}, {y}")
+            kills += 1
     pygame.draw.circle(win, (255, 0, 0), (388, 445), 7)
     pygame.display.flip()
 
 
+def move(dirn):
+    global bg_pos, persons
+    if dirn == "left":
+        if bg_pos[0] < 0:
+            bg_pos[0] += move_speed
+    elif dirn == "right":
+        if bg_pos[0] > (bg.get_width() - W) * -1:
+            bg_pos[0] -= move_speed
+    elif dirn == "up":
+        if bg_pos[1] < 0:
+            bg_pos[1] += move_speed
+    elif dirn == "down":
+        if bg_pos[1] > (bg.get_height() - H) * -1:
+            bg_pos[1] -= move_speed
+
+
 def redraw(win, persons):
     win.blit(bg, bg_pos)
-    win.blit(player_img, (300, H - player_img.get_height()))
     for person in persons:
-        person.draw(win)
-    win.blit(font.render(f"Kills: {KILLS}", 1, (255, 255, 255)), (10, 10))
+        person.draw(win, bg_pos)
+    win.blit(player_img, (300, H - player_img.get_height()))
+    win.blit(font.render(f"Kills: {kills}", 1, (255, 255, 255)), (10, 10))
+    loc = font.render(f"Location: {abs(bg_pos[0]) + 388}, {abs(bg_pos[1]) + 445}", 1, (255, 255, 255))
+    win.blit(loc, (W - loc.get_width() - 10, 10))
     pygame.display.flip()
 
 
@@ -48,24 +72,20 @@ def main():
                 pygame.quit()
                 quit()
             if event.type == USEREVENT + 1:
-                x = random.randint(0, bg.get_width())
-                y = random.randint(0, bg.get_height())
+                x = random.randint(388, 1636)
+                y = random.randint(445, 677)
                 persons.append(Person(x, y, person_img))
                 print(f"New person added at {x}, {y}")
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            if bg_pos[0] < 0:
-                bg_pos[0] += move_speed
+            move("left")
         if keys[pygame.K_RIGHT]:
-            if bg_pos[0] > (bg.get_width() - W) * -1:
-                bg_pos[0] -= move_speed
+            move("right")
         if keys[pygame.K_UP]:
-            if bg_pos[1] < 0:
-                bg_pos[1] += move_speed
+            move("up")
         if keys[pygame.K_DOWN]:
-            if bg_pos[1] > (bg.get_height() - H) * -1:
-                bg_pos[1] -= move_speed
+            move("down")
         if keys[pygame.K_SPACE]:
             shoot(abs(bg_pos[0]) + 388, abs(bg_pos[1]) + 445, persons)
         redraw(win, persons)
